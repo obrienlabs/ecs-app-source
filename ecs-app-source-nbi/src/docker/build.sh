69c1de7e87df0327c40577ab426f54f48f641efe
Michael O'Brien <michael@obrienlabs.org>
2020-02-14 11:29:18 -0500

    #1 ECS-13: upgrade spring boot from 2.1.3 to 2.2.4

--- ecs-app-source-nbi/src/docker/build.sh
+++ ecs-app-source-nbi/src/docker/build.sh
@@ -15,12 +15,21 @@ cp ../../target/*.jar $TARGET_DIR
 cp DockerFile $TARGET_DIR
 cp startService.sh $TARGET_DIR
 cd $TARGET_DIR
-docker build --no-cache --build-arg build-id=$BUILD_ID -t $CONTAINER_IMAGE -f DockerFile .
-docker tag $CONTAINER_IMAGE:latest $CONTAINER_IMAGE:latest
+docker build --no-cache --build-arg build-id=$BUILD_ID -t obrienlabs/$CONTAINER_IMAGE -f DockerFile .
+#docker tag $CONTAINER_IMAGE:latest $CONTAINER_IMAGE:latest
+docker tag obrienlabs/$CONTAINER_IMAGE obrienlabs/$CONTAINER_IMAGE:0.0.2
+# dockerhub
+docker push obrienlabs/$CONTAINER_IMAGE:0.0.2
+# locally
 docker stop $CONTAINER_IMAGE
 docker rm $CONTAINER_IMAGE
 echo "starting: $CONTAINER_IMAGE"
-docker run --name $CONTAINER_IMAGE -d -p 8888:8080 -e os.environment.configuration.dir=/ -e os.environment.ecosystem=sbx $CONTAINER_IMAGE:latest
+docker run --name $CONTAINER_IMAGE \
+    -d -p 8888:8080 \
+    -e os.environment.configuration.dir=/ \
+    -e os.environment.ecosystem=sbx \
+    obrienlabs/$CONTAINER_IMAGE:0.0.2
+
 
 cd ../../src/docker
 
--- ecs-app-source-nbi/src/main/java/systems/cloudlift/nbi/ApiController.java
+++ ecs-app-source-nbi/src/main/java/systems/cloudlift/nbi/ApiController.java
@@ -2,6 +2,8 @@ package systems.cloudlift.nbi;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,11 +19,48 @@ public class ApiController {
 	private static final CharSequence PASS = "PASS";
 	private static final CharSequence FAIL = "FAIL";
     
-    @RequestMapping(method=RequestMethod.GET)
-    public @ResponseBody Api process(@RequestParam(value="action", required=true, defaultValue="undefined") String action) {
+   /* @RequestMapping(method=RequestMethod.GET)
+    public @ResponseBody Api process(@RequestParam(
+    		value="action", required=true, defaultValue="undefined") String action,
+    		 HttpServletRequest request) {
     	String message = PASS.toString();
+    	message = message +  " remoteAddr: " +
+        	request.getRemoteAddr() + " localAddr: " + 
+    		request.getLocalAddr() + " remoteHost: " +
+        	request.getRemoteHost() + " serverName: " + 
+    		request.getServerName();
      	Api api = new Api(counter.incrementAndGet(), message);
-    	System.out.println(counter + " " + this.getClass().getCanonicalName());
+     	
+    	System.out.println(counter + " " + this.getClass().getCanonicalName() + " remoteAddr: " +
+        	request.getRemoteAddr() + " localAddr: " + 
+    		request.getLocalAddr() + " remoteHost: " +
+        	request.getRemoteHost() + " serverName: " + 
+    		request.getServerName());// + " requestedSessionId: " + 
+        	//request.getRequestedSessionId() + " X-FORWARDED-FOR: " +
+    		//request.getHeader("X-FORWARDED-FOR"));
+
+    	return api;
+    } */
+    
+    @RequestMapping(method=RequestMethod.GET)
+    public @ResponseBody Api process(@RequestParam(
+    		value="action", required=true, defaultValue="undefined") String action,
+    		 HttpServletRequest request) {
+    	String message = PASS.toString();
+    	message = message +  " remoteAddr: " +
+        	request.getRemoteAddr() + " localAddr: " + 
+    		request.getLocalAddr() + " remoteHost: " +
+        	request.getRemoteHost() + " serverName: " + 
+    		request.getServerName();
+     	Api api = new Api(counter.incrementAndGet(), message);
+     	
+    	System.out.println(counter + " " + this.getClass().getCanonicalName() + " remoteAddr: " +
+        	request.getRemoteAddr() + " localAddr: " + 
+    		request.getLocalAddr() + " remoteHost: " +
+        	request.getRemoteHost() + " serverName: " + 
+    		request.getServerName());// + " requestedSessionId: " + 
+        	//request.getRequestedSessionId() + " X-FORWARDED-FOR: " +
+    		//request.getHeader("X-FORWARDED-FOR"));
 
     	return api;
     } 
